00001db8c7c4b25b8e623459e5300680933db64b
barrygrubb <barrygrubb83@gmail.com>
2016-05-01 14:19:49 +0100

    Implemented adding of arbitrary bonus values for strikes and spares

--- src/Frame.js
+++ src/Frame.js
@@ -4,6 +4,37 @@ function Frame() {
   this.throw2Score = null;
   this.totalScore = null;
   this.totalScoreType = 'normal';
-  this.bonus = 0;
+  this.bonus = null;
   this.frameComplete = false;
+  this.waitingInitiatedAtThrow = null;
+};
+
+Frame.prototype.watchForBonusThrows = function (game) {
+  this.waitingInitiatedAtThrow = game.numberOfThrows;
+  console.log("watching for bonus")
+};
+
+Frame.prototype.addBonusThrows = function (game) {
+  console.log("made it to addBonusThrows function")
+  // var game = game;
+  if (!this.frameComplete) {
+    // console.log("layer 1")
+    if (this.totalScoreType === "strike") {
+      // console.log("layer 2 strike")
+      if (game.numberOfThrows === (this.waitingInitiatedAtThrow + 2)) {
+        // this.bonus = (game.getNextFrame().throw1Score + game.getNextFrame().throw2Score);
+        console.log("strike bonus")
+        this.bonus = 2;
+        this.frameComplete = true;
+      }
+    } else if (this.totalScoreType === "spare") {
+      console.log("layer 2 spare")
+        if (game.numberOfThrows === (this.waitingInitiatedAtThrow + 1)) {
+          // this.bonus = game.getNextFrame().throw1Score;
+          console.log("spare bonus")
+          this.bonus = 1;
+          this.frameComplete = true;
+        }
+      }
+  }
 };
--- src/Game.js
+++ src/Game.js
@@ -11,23 +11,33 @@ Game.prototype.throw = function() {
     if (this._getCurrentFrame().throw1Score === null) {
       this._getCurrentFrame().throw1Score = this._knockDownSkittles();
       this._getCurrentFrame().pinsRemaining -= this._getCurrentFrame().throw1Score;
+      this._updateBonuses();
+
       if (this._noPinsRemaining()) {
         this._getCurrentFrame().totalScoreType = 'strike';
+        this._getCurrentFrame().watchForBonusThrows(this);
         this._calculateTotalScore();
+        this._updateBonuses();
         this._advanceFrame();
       }
     }
     else {
       this._getCurrentFrame().throw2Score = this._knockDownSkittles();
       this._getCurrentFrame().pinsRemaining -= this._getCurrentFrame().throw2Score;
+      this._updateBonuses();
+
       if (this._noPinsRemaining()) {
         this._getCurrentFrame().totalScoreType = 'spare';
+        this._getCurrentFrame().watchForBonusThrows(this);
       }
       this._calculateTotalScore();
+      this._updateBonuses();
       this._advanceFrame();
     }
   }
   this._incrementThrows();
+  // this._updateBonuses();
+  // this._advanceFrame();
 };
 
 Game.prototype._rackup = function () {
@@ -44,6 +54,10 @@ Game.prototype._getCurrentFrame = function () {
   return this.frameArray[this.currentFrame-1]
 };
 
+Game.prototype.getNextFrame = function () {
+  return this.frameArray[this.currentFrame]
+};
+
 Game.prototype._isFinalFrame = function () {
   this.currentFrame === 10;
 };
@@ -57,9 +71,17 @@ Game.prototype._calculateTotalScore = function () {
 };
 
 Game.prototype._noPinsRemaining = function () {
-  this._getCurrentFrame().pinsRemaining === 0;
+  return this._getCurrentFrame().pinsRemaining === 0;
 };
 
 Game.prototype._incrementThrows = function () {
   this.numberOfThrows++;
+};
+
+Game.prototype._updateBonuses = function () {
+  for (index in this.frameArray) {
+    this.frameArray[index].addBonusThrows(this);
+    // frame.addBonusThrows();
+    // console.log(frame.pinsRemaining)
+  }
 };
